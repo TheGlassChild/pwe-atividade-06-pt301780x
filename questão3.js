59e89ec58458b50d4be3789f2bde56cb10f8bea2
Larita <160742572+djulia-jj@users.noreply.github.com>
2024-11-05 18:48:12 -0300

    Create questão3.js

---
+++ questão3.js
@@ -0,0 +1,12 @@
+let nome = "Marcelo Agante";
+let nota1 = 7;
+let nota2 = 8;
+
+let multNota1 = nota1 * 0.6;
+let multNota2 = nota2 * 0.4;
+let somaNotas = multNota1 + multNota2;
+
+alert("Nome do aluno: " + nome)
+alert("Nota 1: " + nota1)
+alert("Nota 2: " + nota2)
+alert("Nota total: " + somaNotas)
